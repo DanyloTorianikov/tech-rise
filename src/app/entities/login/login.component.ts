981d0bc74855075ad1e084cc8c819085c26691f5
DanyloTorianikov <danylo.torianikov.dev@gmail.com>
2022-12-20 18:20:28 +0200

    Added error interceptor; Added jwt interceptor

--- src/app/entities/login/login.component.ts
+++ src/app/entities/login/login.component.ts
@@ -2,8 +2,9 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EMAIL_PATTERN } from '@constants/email-pattern.constant';
 import { AuthService } from '@services/auth.service';
+import { UserService } from '@services/user.service';
 import { EButtonColor, EButtonSize } from '@shared/button/enums/button.enum';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, switchMap, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -19,7 +20,8 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService, 
+    private userService: UserService
   ) { }
 
   public ngOnInit(): void {
---
+++ src/app/interceptors/error.interceptor.ts
@@ -0,0 +1,33 @@
+import { Injectable, Injector } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from '@angular/common/http';
+import {
+  Observable,
+  throwError
+} from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { AlertService } from '@services/alert.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(
+    private alertService: AlertService,
+  ) { }
+
+  public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request)
+      .pipe(catchError(err => {
+        this.alertService.onError(err.error.message);
+        return throwError(err);
+      }));
+  }
+}
